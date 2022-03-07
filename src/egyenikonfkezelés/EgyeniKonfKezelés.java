5fc3e0cb2897ddef7953f683df1f4f1c5e15be7b
janos0348 <78791817+janos0348@users.noreply.github.com>
2022-03-07 12:25:26 +0100

    3.nev
    deklarálás

--- src/egyenikonfkezelés/EgyeniKonfKezelés.java
+++ src/egyenikonfkezelés/EgyeniKonfKezelés.java
@@ -4,10 +4,10 @@ package egyenikonfkezelés;
 public class EgyeniKonfKezelés {
 
     public static void main(String[] args) {
-
+        int a;
        String nev1 = "KJ";
        String nev2 = "valami";
-
+        int b;
        
     }
     
